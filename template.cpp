fb3f6eee3a880235316179619cd023feb341d1ae
junhup <junhup.lim@u.nus.edu>
2021-06-29 21:19:05 +0800

    add template cpp

--- template.cpp
+++ template.cpp
@@ -6,7 +6,6 @@ using namespace std;
 #define ll long long
 #define si(x)	scanf("%d",&x)
 #define sl(x)	scanf("%lld",&x)
-#define ss(s)	scanf("%s",s)
 #define pi(x)	printf("%d\n",x)
 #define pl(x)	printf("%lld\n",x)
 #define ps(s)	printf("%s\n",s)
@@ -28,17 +27,8 @@ typedef vector<ll>		vl;
 typedef vector<pii>		vpii;
 typedef vector<pl>		vpl;
 typedef vector<vi>		vvi;
-typedef vector<vl>		vvl;
-mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
-int rng(int lim) {
-	uniform_int_distribution<int> uid(0,lim-1);
-	return uid(rang);
-}
-int mpow(int base, int exp);
-void ipgraph(int n, int m);
-void dfs(int u, int par);
 
-const int mod = 1'000'000'007;
+const int mod = 1000000007;
 const int N = 3e5, M = N;
 //=======================
 
@@ -61,33 +51,3 @@ int main() {
 
     return 0;
 }
-
-int mpow(int base, int exp) {
-  base %= mod;
-  int result = 1;
-  while (exp > 0) {
-    if (exp & 1) result = ((ll)result * base) % mod;
-    base = ((ll)base * base) % mod;
-    exp >>= 1;
-  }
-  return result;
-}
-
-void ipgraph(int n, int m){
-	int i, u, v;
-	while(m--){
-		cin>>u>>v;
-    u--, v--;
-		g[u].pb(v);
-		g[v].pb(u);
-	}
-}
-
-void dfs(int u, int par){
-	for(int v:g[u]){
-		if (v == par) continue;
-		dfs(v, u);
-	}
-}
-
-
