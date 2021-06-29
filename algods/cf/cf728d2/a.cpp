dee5bd6fb553836846e75a9d9b71d67688c1d642
junhup <junhup.lim@u.nus.edu>
2021-06-29 21:19:25 +0800

    add cf728d2

---
+++ algods/cf/cf728d2/a.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+
+const int mod = 1000000007;
+const int N = 3e5, M = N;
+//=======================
+
+vi g[N];
+int a[N];
+
+void solve() {
+  int i, j, n, m;
+	bool even;
+	cin>>n;
+	even = n % 2 == 0 ? true : false;
+	if (even) {
+		fo(i, n)
+		if ((i+1) % 2 == 1) {
+			cout << i+2 << " ";
+		} else {
+			cout << i << " ";
+		}
+	} else {
+		cout << "3 1 2 ";
+		Fo(i,3,n)
+		if ((i+1) % 2 == 0) {
+			cout << i+2 << " ";
+		} else {
+			cout << i << " ";
+		}
+	}
+	cout << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+
+    return 0;
+}
